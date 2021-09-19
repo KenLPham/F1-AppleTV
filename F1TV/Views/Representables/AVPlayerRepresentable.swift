1db716621abc2d94297f8c97a52c0482225ec759
Ken Pham <klpham1016@gmail.com>
2021-09-19 11:55:55 -0400

    #14 Add Stream Menu for tvOS 15

--- F1TV/Views/Representables/AVPlayerRepresentable.swift
+++ F1TV/Views/Representables/AVPlayerRepresentable.swift
@@ -26,19 +26,56 @@ struct AVPlayerControllerRepresentable<OverlayView: View>: UIViewControllerRepre
 	func makeUIViewController (context: Context) -> AVPlayerViewController {
 		let controller = AVPlayerViewController()
 		controller.player = player
-        if let overlay = self.overlayView {
-            controller.customOverlayViewController = UIHostingController(rootView: overlay())
+        if #available(tvOS 15.0, *) {
+            // use videoPlayer menus for iOS 15
+        } else {
+            if let overlay = self.overlayView {
+                controller.customOverlayViewController = UIHostingController(rootView: overlay())
+            }
         }
+        
 		return controller
 	}
 	
 	func updateUIViewController(_ viewController: AVPlayerViewController, context: Context) {
         player.playImmediately(atRate: 1.0)
+        
+        if #available(tvOS 15.0, *) {
+            viewController.transportBarCustomMenuItems = context.environment.videoPlayer.menus
+        }
     }
 }
 
 extension AVPlayerControllerRepresentable where OverlayView == Never {
     init (player: AVPlayer) {
         self.player = player
+    }
+}
+
+// MARK: - Transport Bar Item
+class VideoPlayerModifierData: ObservableObject {
+    var menus = [UIMenu]()
+}
+
+extension VideoPlayerModifierData {
+    struct Key: EnvironmentKey {
+        static var defaultValue = VideoPlayerModifierData()
+    }
+}
+
+extension EnvironmentValues {
+    var videoPlayer: VideoPlayerModifierData {
+        get {
+            self[VideoPlayerModifierData.Key.self]
+        }
+        set {
+            self[VideoPlayerModifierData.Key.self] = newValue
+        }
+    }
+}
+
+extension View {
+    func viewPlayerMenus (_ menus: [UIMenu]) -> some View {
+        self.environment(\.videoPlayer.menus, menus)
     }
 }
--- F1TV/Views/Video/StreamView.swift
+++ F1TV/Views/Video/StreamView.swift
@@ -14,16 +14,57 @@ import Kingfisher
 struct StreamView: View {
     @ObservedObject var viewModel: StreamViewModel
     
-    /// - TODO: this should probably just be the screen size
+    var circuitStreams: [AdditionalStream]? {
+        viewModel.channels?.metadata.additionalStreams?.filter { $0.type == .circuit }
+    }
+    
+    var driverStreams: [AdditionalStream]? {
+        viewModel.channels?.metadata.additionalStreams?.filter { $0.type == .driver }
+    }
+    
+    @available(tvOS 15.0, *)
+    var circuitMenu: UIMenu {
+        // - TODO: use the UIAction identifier to figure out which streams are playing
+        var options = circuitStreams?.map { stream in
+            UIAction(title: stream.title, state: .off) { action in
+                viewModel.loadStream(stream)
+            }
+        } ?? []
+        options.append(UIAction(title: "MAIN", state: .off, handler: { action in
+            viewModel.load()
+        }))
+        return UIMenu(options: [ .singleSelection, .displayInline ], children: options)
+    }
+    
+    @available(tvOS 15.0, *)
+    var driverMenu: UIMenu {
+        let options = driverStreams?.map { stream in
+            UIAction(title: stream.title, state: .off) { action in
+                viewModel.loadStream(stream)
+            }
+        } ?? []
+        return UIMenu(title: "Drivers", options: [ .singleSelection, .displayInline ], children: options)
+    }
+    
+    // - TODO: this should probably just be the screen size
     let width: CGFloat = 3840
     let height: CGFloat = 2160
 	
-	/// - TODO: figure out how to keep stream time the same when switching between streams (once we get to that)
+	// - TODO: figure out how to keep stream time the same when switching between streams (once we get to that)
     var body: some View {
         VStack {
             Group {
                 if let player = viewModel.player {
-                    AVPlayerControllerRepresentable(player: player, overlay: Lazy(AdditionalStreamView(viewModel: viewModel)))
+                    if #available(tvOS 15.0, *) {
+                        AVPlayerControllerRepresentable(player: player)
+                            .viewPlayerMenus([
+                                UIMenu(title: "Stream", image: UIImage(systemName: "video"), children: [
+                                    circuitMenu, driverMenu
+                                ])
+                            ])
+                    } else {
+                        AVPlayerControllerRepresentable(player: player, overlay: Lazy(AdditionalStreamView(viewModel: viewModel)))
+                    }
                 } else {
                     KFImage(viewModel.content.pictureUrl(width: width, height: height)).resizable().scaledToFill()
                 }
