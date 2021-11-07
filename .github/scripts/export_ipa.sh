9c7869c4c250d7df7f1d5a952dc0f7fcb6415b0a
Ken Pham <klpham1016@gmail.com>
2021-11-07 10:04:51 -0500

    Github Action Fixes

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -5,6 +5,6 @@ mkdir Payload
 # copy app file
 cp -r build/F1TV.xcarchive/Products/Applications/F1TV.app Payload
 # compress archive to IPA
-zip F1TV.ipa Payload
+zip -r F1TV.ipa Payload
 # delete temp file
 rm -rf Payload
